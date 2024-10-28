0838dfd7f2332b93f5955e8a5db675f47c66d998
Raptor <panraptor13@gmail.com>
2024-10-28 22:58:20 +0100

    cries in programmer

--- Assets/Scripts/BadIng.cs
+++ Assets/Scripts/BadIng.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,15 @@ using UnityEngine;
 public class BadIng : MonoBehaviour
 {
     private FallingObject fallingObject;
+    [SerializeField] private Vector3 fixTheFuckingRotation;
 
     private void Awake()
     {
         fallingObject = GetComponent<FallingObject>();
+    }
+
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(fixTheFuckingRotation);
     }
 }
